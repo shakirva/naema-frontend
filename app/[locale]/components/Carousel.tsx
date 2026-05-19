c8b54cff3fa97f88272cbefdb523852ab9dd6ba0
Rana <ranabdulsalam@gmail.com>
2026-05-19 12:24:09 +0530

    translate hero and marque

--- app/[locale]/components/Carousel.tsx
+++ app/[locale]/components/Carousel.tsx
@@ -1,24 +1,28 @@
+
+
 import React from "react";
+import { useTranslations } from "next-intl";
 
 const badges = [
-  "Farm to Table",
-  "Premium Quality",
-  "Fast Delivery",
-  "Gift Packaging",
+  "farmToTable",
+  "premiumQuality",
+  "fastDelivery",
+  "giftPackaging",
 ];
 
 const Carousel = () => {
+  const t = useTranslations("Carousel");
 
   const displayBadges = [...badges, ...badges, ...badges];
 
   return (
-    <section className="w-full bg-cream px-16 py-8  border-black/10">
+    <section className="w-full bg-cream px-16 py-8 border-black/10">
       <div className="w-full overflow-hidden whitespace-nowrap">
         <div className="carousel-anim flex items-center w-fit will-change-transform">
           {displayBadges.map((item, i) => (
             <div key={i} className="flex items-center">
               <span className="font-serif capitalize text-3xl text-black/80 px-8">
-                {item}
+                {t(item)}
               </span>
 
               <span className="size-[6px] bg-black/80 rounded-full shrink-0" />

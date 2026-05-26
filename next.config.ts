3b7f0706609ebec3165d0b4e810fad4826a319c7
MUHAMMED SHAKIR VA <111525692+shakirva@users.noreply.github.com>
2026-05-27 03:23:10 +0530

    Allow remote image domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '187.127.166.159',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
